257375c958c07cd1616fde1ab5758d9be3852422
Nejc <nmursi2@gmail.com>
2024-05-07 20:54:20 +0800

    Revert "fix: xss"
    This reverts commit dabb407b70c8fa9552b1cbfe86f16ec3a5a380e9.

--- theme/src/gatsby-theme-docz/helpers/index.js
+++ theme/src/gatsby-theme-docz/helpers/index.js
@@ -3,15 +3,11 @@ export const activeEnv =
 
 export const isUrl = (str) => {
   try {
-    const url = new URL(str);
-    // Check if the protocol is HTTP or HTTPS to prevent XSS attacks
-    // Adjust this list to your needs (e.g., you might want to allow 'ftp:' or 'mailto:')
-    const allowedProtocols = ['http:', 'https:'];
-    return allowedProtocols.includes(url.protocol);
+    return Boolean(new URL(str));
   } catch (e) {
     return false;
   }
-};  
+};
 
 export const removeSlashes = (str) => str.replace(/^\/|\/$/g, '');
 
